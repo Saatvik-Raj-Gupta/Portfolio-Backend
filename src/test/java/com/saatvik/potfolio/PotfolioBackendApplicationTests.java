470b1107df166c8e37746c91085b9822acbc81f5
Saatvik Raj Gupta <saatvik.goku@gmail.com>
2026-01-10 19:56:55 +0530

    Initialized the project with Spring Initializer.

---
+++ src/test/java/com/saatvik/potfolio/PotfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saatvik.potfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
